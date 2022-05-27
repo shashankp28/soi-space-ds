934cd7faec9cef430b319eb9f20d35b2c0ed102f
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-05-27 20:13:59 +0530

    updated show.py to view data in a customized way

--- GUI/services/show.py
+++ GUI/services/show.py
@@ -4,3 +4,14 @@ import streamlit as st
 def app(data):
     st.dataframe(data)
     st.write('Columns present are ', data.columns.tolist())
+    columns = st.multiselect('Columns', data.columns.tolist()+['All'])
+
+    if 'All' in columns:
+        columns = data.columns.tolist()
+
+    st.write('No.of rows present are',data.shape[0])
+    customized_data = data[columns]
+    start = st.number_input("Select start row",step=1,min_value=1,max_value=data.shape[0])
+    end = st.number_input("Select end row",step=1,min_value=1,max_value=data.shape[0])
+    customized_data = customized_data.iloc[start-1:end]
+    st.dataframe(customized_data)
