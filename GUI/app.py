aa74d95449978f8fef580a3cdf155c9d169bb5e2
Shashank P <shashankp2832@gmail.com>
2022-06-06 20:28:00 +0530

    New pages

--- GUI/app.py
+++ GUI/app.py
@@ -1,28 +1,24 @@
-from multiprocessing.sharedctypes import Value
 import streamlit as st
 import pandas as pd
-from multiapp import MultiApp
+from services.multiapp import MultiApp
 from functools import partial
-from services import show, plot, iplots
-
-rad = st.sidebar.radio("Tool or Help", ['Tool', 'Help'])
+from pages import show, analysis, help, custom, download, three_d
 
 
 def refresh():
-    rad = "Tool"
+    pass
 
 
-if rad == "Tool":
-    file = st.file_uploader('Upload CSV file',
-                            type='csv', help="Format")
-    if file is not None:
-        st.button("Refresh", on_click=refresh)
-        data = pd.read_csv(file)
-        app = MultiApp()
-        app.add_app('Show data', partial(show.app, data))
-        app.add_app('Plot data', partial(plot.app, data))
-        app.add_app('Plots', iplots.app)
-        app.run()
-
-if rad == "Help":
-    st.header("For now this is help")
+file = st.file_uploader('Upload CSV file',
+                        type='csv', help="Format")
+if file is not None:
+    st.button("Refresh", on_click=refresh)
+    data = pd.read_csv(file)
+    app = MultiApp()
+    app.add_app('Show data', partial(show.app, data))
+    app.add_app('Analysis', partial(analysis.app, data))
+    app.add_app('Custom Plots', custom.app)
+    app.add_app('3-D Plots', three_d.app)
+    app.add_app('Download', download.app)
+    app.add_app('Help', help.app)
+    app.run()
