d56b0b24c7cda9558747d3244b06d93f7b46543c
Arvind Kumar M <200020008@iitdh.ac.in>
2022-06-08 23:09:12 +0530

    Added csv format checking

--- GUI/app.py
+++ GUI/app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
+from services.services import check_format
 from services.multiapp import MultiApp
 from functools import partial
 from pages import show, analysis, help, custom, download, three_d
-
 
 def refresh():
     pass
@@ -18,14 +18,18 @@ if file:
     # Prediction
     st.button("Refresh", on_click=refresh)
     data = pd.read_csv(file)
-    app = MultiApp()
-    app.add_app('Show data', partial(show.app, data))
-    app.add_app('Analysis', analysis.app)
-    app.add_app('Custom Plots', custom.app)
-    app.add_app('3-D Plots', partial(three_d.app, data))
-    app.add_app('Download', partial(download.app, data, file.name))
-    app.add_app('Help', help.app)
-    app.run()
+    format_status, error_msg = check_format(data)
+    if(format_status):
+        app = MultiApp()
+        app.add_app('Show data', partial(show.app, data))
+        app.add_app('Analysis', analysis.app)
+        app.add_app('Custom Plots', custom.app)
+        app.add_app('3-D Plots', partial(three_d.app, data))
+        app.add_app('Download', partial(download.app, data, file.name))
+        app.add_app('Help', help.app)
+        app.run()
+    else:
+        st.write(error_msg)
 else:
     st.write("Format to be uploaded")
     # @Arvind
--- GUI/services/services.py
+++ GUI/services/services.py
@@ -1,5 +1,41 @@
-def check(df):
-    return (True, False)
+import re
+
+
+def check_format(df):
+    correct = True
+    error_msg = ""
+    required_columns = {'kepid' : 0, 'tce_plnt_num' : 0, 'tce_rogue_flag' : 0, 'tce_period' : 0,
+       'tce_period_err' : 0, 'tce_time0bk' : 0, 'tce_time0bk_err' : 0, 'tce_impact' : 0,
+       'tce_impact_err' : 0, 'tce_duration' : 0, 'tce_duration_err' : 0, 'tce_depth' : 0,
+       'tce_depth_err' : 0, 'tce_model_snr' : 0, 'tce_prad' : 0, 'tce_prad_err' : 0, 'tce_eqt' : 0,
+       'tce_eqt_err' : 0, 'tce_insol' : 0, 'tce_insol_err' : 0, 'tce_steff' : 0,
+       'tce_steff_err' : 0, 'tce_slogg' : 0, 'tce_slogg_err' : 0, 'tce_sradius' : 0,
+       'tce_sradius_err' : 0}
+    
+    columns = df.columns
+
+    for i in columns:
+        if(i not in required_columns.keys()):
+            df.drop(i, inplace=True, axis=1)
+        
+        else:
+            if(required_columns[i] == 0):
+                required_columns[i] = 1
+            else:
+                df.drop(i, inplace=True, axis=1)  
+
+    for i in required_columns:
+        if(required_columns[i] == 0):
+            error_msg = i + " column is missing"
+            return False, error_msg
+    
+    types = df.dtypes
+    for i in required_columns:
+        if(types[i] != 'float' and types[i] != 'int'):
+            error_msg = "Data type of " + str(i) + " should be int or float " 
+            return  False, error_msg
+
+    return True, error_msg
 
 
 def predict(df):
