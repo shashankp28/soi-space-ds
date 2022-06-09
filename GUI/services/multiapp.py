3d8c6edfcbef41c8b81856565e2d195e19621661
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-06-09 20:31:12 +0530

    Update multiapp.py

--- GUI/services/multiapp.py
+++ GUI/services/multiapp.py
@@ -17,8 +17,22 @@ class MultiApp:
         return self.titles.index(title)
 
     def run(self):
-        app = st.selectbox(
-            'Navigation',
+        st.markdown(""" <div role=nav_head>
+           <h3>Navigation</h3></div> """, unsafe_allow_html=True)
+        app = st.radio('',
             self.titles)
-
+        st.markdown(""" <style>
+            div[role="radiogroup"]
+            {
+                position: absolute !important;
+                bottom: 40px !important;
+                left: -380px !important;
+            }
+            div[role="nav_head"]
+            {
+                position: absolute !important;
+                bottom: 140px !important;
+                left: -380px !important;
+            }
+            footer {visibility: hidden;}</style> """, unsafe_allow_html=True)
         self.apps[self.get_index(app)]()
