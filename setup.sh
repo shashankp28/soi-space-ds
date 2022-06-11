56f691bda8cb7319a869e359618dc43c668c6986
Shashank P <shashankp2832@gmail.com>
2022-06-11 14:53:12 +0530

    setup.sh and readme changes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,12 @@ sudo apt-get upgrade
 while true; do
     read -p "Do you wish to setup up a vitual environment to install packages? [Y/n] " yn
     case $yn in
-        [Yy]* ) python3 -m venv .env && source .env/bin/activate; break;;
+        [Yy]* ) { 
+                    python3 -m venv .env
+                } || { 
+                    python -m venv .env
+                }; 
+                source .env/bin/activate; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
